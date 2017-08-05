d93eb916f2b4f14f90855be09875e582d8fbb0c9
Adam Paterson <adam.paterson03@gmail.com>
2017-08-05 05:29:01 +0000

    Added jump to a point in the video

--- content/src/scripts/components/app/App.jsx
+++ content/src/scripts/components/app/App.jsx
@@ -4,18 +4,34 @@ import {connect} from 'react-redux';
 class App extends Component {
   constructor(props) {
     super(props);
-  }
+    this.state = {value: ''};
+    this.skipToTime = this.skipToTime.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+   }
+
+   handleChange(event) {
+     this.setState({value: event.target.value});
+   }
+
+   handleSubmit(event) {
+     event.preventDefault();
+     this.skipToTime(this.state.value)
+   }
 
   componentDidMount() {
     console.log('yay it mounted')
   }
-
+  skipToTime() {
+    document.getElementsByTagName('video')[0].currentTime = this.state.value;
+    document.getElementsByTagName('video')[1].currentTime = this.state.value;
+  }
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label>
           Name:
-          <input type="text" name="name" />
+          <input type="text" name="name" value={this.state.value} onChange={this.handleChange}/>
         </label>
         <input type="submit" value="Submit" />
       </form>
