556e1a9472cf969e65935efcaa20655df96b494c
Manan <manan_rana@hotmail.com>
2017-08-05 17:40:10 +1000

    made a few css changes

--- content/src/scripts/components/app/App.jsx
+++ content/src/scripts/components/app/App.jsx
@@ -6,11 +6,14 @@ import styled from 'styled-components';
 const Title = styled.h1`
 	font-size: 1.5em;
 	text-align: center;
-	color: palevioletred;
+  background: -webkit-linear-gradient( 135deg, #43CBFF 10%, #9708CC 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 `;
 
 const Input = styled.input`
-	padding: 0.5em;
+  padding: 1em;
+  padding-right: 20em;
 	margin: 0.5em;
 	color: palevioletred;
 	background: papayawhip;
